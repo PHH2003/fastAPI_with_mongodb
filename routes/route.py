97290823cdbae710bfdb6a7de39e675ad8e04bbd
hiepph <phanhuyhiep2k3@gmail.com>
2024-04-11 16:11:31 +0700

    commit CRUD

---
+++ routes/route.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from models.todos import Todo
+from config.database import collection_name
+from schema.schemas import list_serial
+from bson import ObjectId
+
+router = APIRouter()
+
+#GET Request Method
+@router.get("/")
+async def get_todos():
+    todos = list_serial(collection_name.find())
+    return todos
+
+# POST Request Method
+@router.post("/")
+async def post_todos(todo: Todo):
+    collection_name.insert_one(dict(todo))
+
+#PUT Request Method
+@router.put("/{id}")
+async def put_todos(id:str, todo:Todo):
+    collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(todo)})
+
+#Delete Request Method
+@router.delete("/{is}")
+async def delete_todos(id: str, todo: Todo):
+    collection_name.find_one_and_delete({"_id": ObjectId(id)})
